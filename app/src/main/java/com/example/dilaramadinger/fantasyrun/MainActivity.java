be889f336c6d4cd918ce27d630a963ab0945aedc
cudilara <dilara.madinger@colorado.edu>
2018-07-24 16:24:23 -0600

    basic button

--- app/src/main/java/com/example/dilaramadinger/fantasyrun/MainActivity.java
+++ app/src/main/java/com/example/dilaramadinger/fantasyrun/MainActivity.java
@@ -5,7 +5,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Button hook up
+        // https://developer.android.com/reference/android/widget/Button
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast.makeText(getApplicationContext(), "Button pressed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
